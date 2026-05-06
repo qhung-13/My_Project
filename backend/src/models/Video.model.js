2fde7d70160bb81c4cbd2029ad14afcedd7be9ce
Switer <hungtranpc1342k6@gmail.com>
2026-05-06 10:28:52 +0700

    feat: add Video schema

---
+++ backend/src/models/Video.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      default: null,
+    },
+    thumbnailUrl: {
+      type: String,
+      default: null,
+    },
+    duration: {
+      type: Number,
+      default: 0,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    tags: [{ type: String }],
+    status: {
+      type: String,
+      enum: ["public", "private", "processing"],
+      default: "processing",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Video = mongoose.model("Video", videoSchema);
+export default Video;
