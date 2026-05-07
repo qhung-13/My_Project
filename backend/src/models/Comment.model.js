fde92f11386443b3ce06d2e4dca5987bb778a6d4
Switer <hungtranpc1342k6@gmail.com>
2026-05-07 09:00:30 +0700

    feat: add Comment schema

---
+++ backend/src/models/Comment.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    videoId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    likesCount: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+export default Comment;
