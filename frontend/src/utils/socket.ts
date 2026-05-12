39685c1194c6ab23f77381e613acf85d1c5ce736
Switer <hungtranpc1342k6@gmail.com>
2026-05-12 10:07:16 +0700

    fix: correct socket URL replace in socket.ts

--- frontend/src/utils/socket.ts
+++ frontend/src/utils/socket.ts
@@ -1,11 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io(
-  import.meta.env.VITE_API_URL?.replace("/api/", "") || "http://localhost:5000",
-  {
-    withCredentials: true,
-    autoConnect: false, // Don't auto connect - just connect when need
-  },
-);
+const socket = io(import.meta.env.VITE_API_URL?.replace("/api", ""), {
+  withCredentials: true,
+  autoConnect: false, // Don't auto connect - just connect when need
+});
 
 export default socket;
