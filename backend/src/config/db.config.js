7138b297bc67c98156944b905f9f86ea4258f292
Quoc Hung <hungtranpc1342k6@gmail.com>
2026-07-17 23:02:30 +0700

    fix(db config): log the error and rethrow

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -9,8 +9,8 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Successfully connected to MongoDB");
   } catch (error) {
-    console.error("MongoDB connection error: ", error.message);
-    process.exit(1);
+    console.error("MongoDB connection error:", error.message);
+    throw error;
   }
 };
 
