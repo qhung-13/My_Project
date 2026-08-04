9735bf38a2ab4ff805416f98ad4cde4a03dc857c
Quoc Hung <hungtranpc1342k6@gmail.com>
2026-08-04 13:56:29 +0700

    fix(axios): update base URL configuration to use API_BASE_URL

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -1,16 +1,9 @@
 import axios from "axios";
+import { API_BASE_URL } from "../config/api";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: API_BASE_URL,
   withCredentials: true,
-});
-
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("authToken");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
 });
 
 export default instance;
